84f9fd6f3c0263d2f7236bbc9a4ef20252b99d9e
fernand002 <fpcasanola@outlook.es>
2024-12-09 20:13:42 +0100

    ss

--- POO/EjerciciosPOO/EjercicioUno/rectangulo.java
+++ POO/EjerciciosPOO/EjercicioUno/rectangulo.java
@@ -15,4 +15,47 @@ public class rectangulo {
 		this.altura = altura;
 
 	}
+	
+	//getters and setters 
+	
+	public double getBase() {
+		return base;
+	}
+
+	public void setBase(double base) {
+		this.base = base;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	//metodos area y perimetro 
+	
+	
+	public double Calcularperimetro () {
+
+		double perimetro = (base * 2) + (altura *2);
+		return perimetro; 
+	}
+	
+	public double CalcularArea() {
+		dauble area = base * altura ; 
+		return area ;
+		
+	}
+	
+	void MostrarPorPantalla() {
+		System.out.println("El area del rectangulo es " + CalcularArea());
+		System.out.println("El perimetro del rectangulo es:" + Calcularperimetro());
+	}
+	
+	
+	
+	
+	
 }
